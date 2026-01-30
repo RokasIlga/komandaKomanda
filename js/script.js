ecea6294dd06a06f410e410b6ed791c65de5093e
Spooky Coder <rainbowragetwo@gmail.com>
2026-01-30 10:14:09 +0200

    Clean up code for changing background color
    Removed unnecessary whitespace and comments.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
 // Code to change color!
-
-    const randomBckBtn = document.querySelector('#color');
-    
-
-    randomBckBtn.addEventListener('click', () => {
-        const firstrandom = Math.floor(Math.random() * 256);
-        const secondrandom = Math.floor(Math.random() * 256);
-        const thirdrandom = Math.floor(Math.random() * 256);
-        document.body.style.backgroundColor = `rgb(${firstrandom}, ${secondrandom}, ${thirdrandom})`;
-    });
-
-
